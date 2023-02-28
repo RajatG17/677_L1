98518b4d7b80eef3491f894d4ecc1b056cb833bf
ShreyaDubey1002 <dubeyshreya1002@gmail.com>
2023-02-27 20:27:30 -0500

    Added a script on client side to run multiple client processes in parallel and a script on server side to run a server.

---
+++ src/part1/run_remote_script_client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+  python3 -u $PWD/lab-1-asterix-and-the-stock-bazaar-677-lab1/src/part1/client/client.py 128.119.243.147 8976 & 
+  python3 -u $PWD/lab-1-asterix-and-the-stock-bazaar-677-lab1/src/part1/client/client.py 128.119.243.147 8976 &
+  python3 -u $PWD/lab-1-asterix-and-the-stock-bazaar-677-lab1/src/part1/client/client.py 128.119.243.147 8976 &
+  python3 -u $PWD/lab-1-asterix-and-the-stock-bazaar-677-lab1/src/part1/client/client.py 128.119.243.147 8976 &
+  python3 -u $PWD/lab-1-asterix-and-the-stock-bazaar-677-lab1/src/part1/client/client.py 128.119.243.147 8976 &
